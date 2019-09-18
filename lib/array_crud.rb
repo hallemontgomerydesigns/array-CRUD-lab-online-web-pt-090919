758ec7cbe27df93b17564a7a4e63a2bd9fea6ef3
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-18 17:34:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  array = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Draco Malfoy"]
- array << "Severus Snape"
+ array.push("Severus Snape")
 end
 
 def add_element_to_start_of_array(array, element)
