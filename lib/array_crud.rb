380385c078c56d32bc80ad375518cc06463cde51
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-18 17:31:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-   ["Harry Potter", "Ron Weasley", "Hermoine Granger"]
+   ["Harry Potter", "Ron Weasley", "Hermione Granger"]
 end
 
 def add_element_to_end_of_array(array, element)
